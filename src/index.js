e9fcde013a38cf8022b6da9adb6fe9ac5bc48fdc
brighton <brighton.chabula@hellofresh.com>
2024-02-18 19:50:49 +0100

    refactor project setup

--- src/index.js
+++ src/index.js
@@ -1,44 +1,24 @@
-import './env.js'
-import { fastify } from 'fastify';
-import fastifyStatic from '@fastify/static'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import Fastify from 'fastify';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const app = Fastify()
 
-//ESM specific features
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// TODO @Daniel: Connect to mongodb database here
+// use the process.env.MONGODB_URI to connect to the database
 
-const app = fastify()
+app.get('/status', async (request, reply) => {
+    return {status: 'ok'}
 
-// Check if MONGO_URL is set and log it
-if (process.env.MONGO_URL) {
-    console.log('MONGO_URL:', process.env.MONGO_URL);
-} else {
-    console.error('MONGO_URL environment variable is not set.');
-}
+})
 
 async function startApp() {
     try {
-
-        app.register(fastifyStatic, {
-            root: path.join(__dirname, "public"),
-        });
-
-        //app.get("/", {}, (request, reply) => {
-        //    reply.send({
-        //        data: "Hello world",
-        //   })
-        //  })
-
-        await app.listen({ port: 3000 })
+        await app.listen({port: 3000})
         console.log('Server listening at port: 3000')
     } catch (e) {
         console.error(e)
-        
     }
 }
 
