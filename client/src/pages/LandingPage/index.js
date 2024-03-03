6a07ece30e1f67fb159f564fd9f82e7f2acbd41b
Daniel Chanda <danielchanda41@gmail.com>
2024-03-03 02:20:47 +0200

    Added some styling to buttons

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import Coffee from "../../components/img/coffee1.png"; // Adjust the path based on the file's location
 
 const LandingPage = () => {
   return (
-    <div>
-        <h1>Landing Page!</h1>  
+    <div className="landing-page-contaner">
+      <img src={Coffee} alt="Coffee" className="logo-image" />
+      <h1 className="landing-page-heading">ConvoCafe</h1>
+      <h3>Offline Chatting Platform</h3>
+        <div>
+          <button className="signup-button">Sign Up</button>
+        </div>
+        <div>
+          <button className="login-button">Log In</button>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
