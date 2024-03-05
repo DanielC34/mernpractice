275a5f464ac60ded4b9ae0c02fd1ffb0063f3cf7
Daniel Chanda <danielchanda41@gmail.com>
2024-03-05 16:04:59 +0200

    Edited Signup page

--- client/src/pages/SignUpPage/SignUp.js
+++ client/src/pages/SignUpPage/SignUp.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log(
+      "Email: ", email,
+      "Username: ", username,
+      "Password: ", password,
+      "Confirm password: ", confirmPassword
+    );
+  };
+
   return (
-    <div>
+    <div className="signup-page">
       <h2>Sign Up</h2>
+      <form>
+        <div>
+          <input
+            type="email"
+            value={email}
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={confirmPassword}
+            placeholder="Confirm Password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Signup
