1d71ce0cc188fb5adb74712c4478cb2de0dcd618
Daniel Chanda <danielchanda41@gmail.com>
2024-03-13 17:50:54 +0200

    Error in Routing

--- client/src/routes/appRoutes.js
+++ client/src/routes/appRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LandingPage from '../pages/LandingPage/LandingPage'
-import Login from "../pages/LoginPage/LogInPage";
+import LogInPage from "../pages/LoginPage/LogInPage";
 import Signup from '../pages/SignUpPage/SignUp'
 import ChatPage from '../pages/ChatPage/ChatPage'
 
@@ -10,7 +10,7 @@ const AppRoutes = () => {
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" Component={LandingPage} />
-                <Route exact path="/login" Component={Login} />
+                <Route exact path="/login" Component={LogInPage} />
                 <Route exact path="/signup" Component={Signup} />
                 <Route exact path="/chat" Component={ChatPage} />
             </Switch>
