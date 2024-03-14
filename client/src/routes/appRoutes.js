01d3de195f60ebb32d94ffe34143ab963360c08c
Daniel Chanda <danielchanda41@gmail.com>
2024-03-14 12:44:00 +0200

    Configuring routes for testing

--- client/src/routes/appRoutes.js
+++ client/src/routes/appRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Router, Route } from "react-router-dom";
 import LandingPage from '../pages/LandingPage/LandingPage'
 import LogInPage from "../pages/LoginPage/LogInPage.js";
 import Signup from '../pages/SignUpPage/SignUp'
@@ -8,12 +8,12 @@ import ChatPage from '../pages/ChatPage/ChatPage'
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Switch>
+            <Router>
                 <Route exact path="/" Component={LandingPage} />
                 <Route exact path="/login" Component={LogInPage} />
                 <Route exact path="/signup" Component={Signup} />
                 <Route exact path="/chat" Component={ChatPage} />
-            </Switch>
+            </Router>
         </BrowserRouter>
     );
 };
