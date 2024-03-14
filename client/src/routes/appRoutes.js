f50e4867427508d30359db7d309cedc2e72d2440
Daniel Chanda <danielchanda41@gmail.com>
2024-03-14 12:50:20 +0200

    Route error corrections

--- client/src/routes/appRoutes.js
+++ client/src/routes/appRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Router, Route } from "react-router-dom";
 import LandingPage from '../pages/LandingPage/LandingPage'
-import LogInPage from "../pages/LoginPage/LogInPage.js";
+import Login from "../pages/LogInPage/LoginInPage";
 import Signup from '../pages/SignUpPage/SignUp'
 import ChatPage from '../pages/ChatPage/ChatPage'
 
@@ -10,7 +10,7 @@ const AppRoutes = () => {
         <BrowserRouter>
             <Router>
                 <Route exact path="/" Component={LandingPage} />
-                <Route exact path="/login" Component={LogInPage} />
+                <Route exact path="/login" Component={Login} />
                 <Route exact path="/signup" Component={Signup} />
                 <Route exact path="/chat" Component={ChatPage} />
             </Router>
