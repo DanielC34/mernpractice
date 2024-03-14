9c5e57aef28abae33080868f1a1cb15afc69f577
Daniel Chanda <danielchanda41@gmail.com>
2024-03-14 13:34:23 +0200

    Rendering corrections

--- client/src/routes/appRoutes.js
+++ client/src/routes/appRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from '../pages/LandingPage/LandingPage'
 import Login from "../pages/LogInPage/LoginInPage";
 import Signup from '../pages/SignUpPage/SignUp'
@@ -7,14 +7,14 @@ import ChatPage from '../pages/ChatPage/ChatPage'
 
 const AppRoutes = () => {
     return (
-        <Router>
-            <Route>
-                <Route exact path="/" Component={LandingPage} />
-                <Route exact path="/login" Component={Login} />
-                <Route exact path="/signup" Component={Signup} />
-                <Route exact path="/chat" Component={ChatPage} />
-            </Route>
-        </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/chat" component={ChatPage} />
+        </Routes>
+      </Router>
     );
 };
 
