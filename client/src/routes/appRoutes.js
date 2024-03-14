ef385578aafbcc38209bd58966557d05105dd901
Daniel Chanda <danielchanda41@gmail.com>
2024-03-14 12:56:51 +0200

    Routing Corrections

--- client/src/routes/appRoutes.js
+++ client/src/routes/appRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import LandingPage from '../pages/LandingPage/LandingPage'
 import Login from "../pages/LogInPage/LoginInPage";
 import Signup from '../pages/SignUpPage/SignUp'
@@ -7,14 +7,14 @@ import ChatPage from '../pages/ChatPage/ChatPage'
 
 const AppRoutes = () => {
     return (
-        <BrowserRouter>
-            <Router>
+        <Router>
+            <Route>
                 <Route exact path="/" Component={LandingPage} />
                 <Route exact path="/login" Component={Login} />
                 <Route exact path="/signup" Component={Signup} />
                 <Route exact path="/chat" Component={ChatPage} />
-            </Router>
-        </BrowserRouter>
+            </Route>
+        </Router>
     );
 };
 
