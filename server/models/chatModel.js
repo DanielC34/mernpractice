6e7ea8885e7b77e3cd6e83417ab7fc829806feab
Daniel Chanda <danielchanda41@gmail.com>
2024-03-18 13:18:13 +0200

    Added comments to models

--- server/models/chatModel.js
+++ server/models/chatModel.js
@@ -1,5 +1,9 @@
 //chatModel defines the structure of the chat info in the database
-//
+// Each chat will include chat name, check if the chat is a one-to-one
+// or if it is a group chat, includes the user(s) participating in the chat
+// keeps track of most recent message sent in the chat
+// In the case of a group chat being involved, the groupAdmin field stores the ID of the chat group admin
+
 
 const mongoose = require('mongoose');
 
