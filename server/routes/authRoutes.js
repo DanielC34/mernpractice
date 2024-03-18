0209895e1a14ad810b6236c84f0ae45a40413f2b
Daniel Chanda <danielchanda41@gmail.com>
2024-03-18 21:53:53 +0200

    Configuring mongoose database connection

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const User = require("../models/chatModel");
+const User = require("../models/userModel");
 
 //Route for handling Sign up requests
 router.post("/signup", async (req, res) => {
